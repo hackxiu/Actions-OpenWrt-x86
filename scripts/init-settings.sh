61e52b58dc9ffeb33ecab0d122bd28d8a51b0914
hackxiu <hackxiu@gmail.com>
2025-03-11 15:55:49 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # 修改lan口ip
-uci set network.lan.ipaddr=10.0.0.2
+uci set network.lan.ipaddr='10.0.0.2'
 # 网关指向上级路由
-uci set network.lan.gateway=10.0.0.1
+uci set network.lan.gateway='10.0.0.1'
 # 设置子网码
-uci set network.lan.netmask=255.255.240.0
+uci set network.lan.netmask='255.255.240.0'
 # dns指向上级路由
-uci set network.lan.dns=10.0.0.1
+uci set network.lan.dns='10.0.0.1'
 # 确认修改
 uci commit network
 # 关闭lan口dhcp
-uci set dhcp.lan.ignore=1
+uci set dhcp.lan.ignore='1'
 uci commit dhcp
 
 # 设置所有网口可访问网页终端
